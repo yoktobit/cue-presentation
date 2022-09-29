089d126140e47345d890995c0e8727524503b44e
Martin <martin@yoktobit.de>
2022-09-29 16:58:15 +0200

    korrektes Loglevel

--- project/main.go
+++ project/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
 	"github.com/yoktobit/cue-project/router"
 )
 
 func main() {
 	e := echo.New()
+	e.Logger.SetLevel(log.INFO)
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
